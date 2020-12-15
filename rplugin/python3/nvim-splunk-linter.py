c742944e8224a54d2cc3ac60e31a92a6bfd97def
Christopher Talib <christopher@talib.me>
2020-12-15 16:12:24 +0100

    first commit

---
+++ rplugin/python3/nvim-splunk-linter.py
@@ -0,0 +1,32 @@
+import re
+
+import pynvim
+
+
+@pynvim.plugin
+class TestPlugin(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.function("LintSplunk")
+    def lintSplunk(self, args):
+        current_line = self.nvim.current.line
+        front = current_line.split("(")
+        i = 0
+        j = 0
+        res = ""
+        for line in front:
+            p = ""
+            if j != 0:
+                p = "("
+            back = re.findall("\)", line)
+            line = ((i) * "\t") + p + line
+            self.nvim.current.buffer.append(line, -1)
+            if len(back) == 0:
+                i = i + 1
+            elif len(back) > 1:
+                i = i - len(back)
+                if i < 0:
+                    i = 0
+            j = j + 1
+        return
